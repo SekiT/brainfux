ec411ac071f878999c96b34e2b551a7ebfe3c726
Takaaki Seki <ormscrongers@gmail.com>
2016-10-09 12:41:11 +0900

    Use ex_doc, dialyxir, credo

--- mix.exs
+++ mix.exs
@@ -2,31 +2,28 @@ defmodule Brainfux.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :brainfux,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :brainfux,
+      version: "0.0.0",
+      elixir: "~> 1.3",
+      name: "Brainfux",
+      deps: deps(),
+      docs: [extras: ["README.md"]],
+      dialyzer: [plt_add_deps: :transitive],
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [
+      applications: []
+    ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:ex_doc  , "~> 0.14.1", only: :dev},
+      {:dialyxir, "~> 0.3.5" , only: :dev},
+      {:credo   , "~> 0.4.12", only: :dev},
+    ]
   end
 end
