85f8e49609df8061b61cb5846ffc646be86852a4
Takaaki Seki <ormscrongers@gmail.com>
2016-10-11 00:17:20 +0900

    Ready to publish

--- mix.exs
+++ mix.exs
@@ -1,13 +1,17 @@
 defmodule Brainfux.Mixfile do
   use Mix.Project
 
+  @github_url "https://github.com/SekiT/brainfux"
+
   def project do
     [
       app: :brainfux,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.3",
       name: "Brainfux",
-      source_url: "https://github.com/SekiT/brainfux",
+      description: description(),
+      package: package(),
+      source_url: @github_url,
       deps: deps(),
       docs: [extras: ["README.md"]],
       dialyzer: [plt_add_deps: :transitive],
@@ -26,6 +30,21 @@ defmodule Brainfux.Mixfile do
       {:dialyxir, "~> 0.3.5" , only: :dev },
       {:credo   , "~> 0.4.12", only: :dev },
       {:meck    , "~> 0.8.4" , only: :test},
+    ]
+  end
+
+  defp description do
+    """
+    Brainfux enables you to define brainfuck function in elixir.
+    """
+  end
+
+  defp package do
+    [
+      name: :brainfux,
+      licenses: ["WTFPL"],
+      maintainers: ["Takaaki Seki"],
+      links: %{"GitHub" => @github_url},
     ]
   end
 end
