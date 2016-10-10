955993580a0b9a5c3c1ea0eb266315c3e7250c38
Takaaki Seki <ormscrongers@gmail.com>
2016-10-10 13:44:25 +0900

    Version 0.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Brainfux.Mixfile do
   def project do
     [
       app: :brainfux,
-      version: "0.0.0",
+      version: "0.1.0",
       elixir: "~> 1.3",
       name: "Brainfux",
       deps: deps(),
